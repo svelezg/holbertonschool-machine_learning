b3f7bfb2d13e962251e7924d2922dc9f4daad8f9
svelezg <svelez.velezgarcia@gmail.com>
2020-04-15 09:26:06 -0500

    Task 5. Across The Planes Check every row length

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -2,32 +2,20 @@
 """Contain add_matrices2D function"""
 
 
-def matrix_shape(matrix):
-    """calculates the shape of a matrix"""
-    if matrix:
-        shape = [len(matrix)]
-        while type(matrix[0]) == list:
-            shape.append(len(matrix[0]))
-            matrix = matrix[0]
-        return shape
-    else:
-        return [0]
-
-
 def add_matrices2D(arr1, arr2):
     """ adds two matrices element-wise"""
 
-    shape1 = matrix_shape(arr1)
-    shape2 = matrix_shape(arr2)
-
-    if shape1[0] != shape2[0] or shape1[1] != shape2[1]:
+    if len(arr1) != len(arr2):
         return None
     else:
         new_matrix = []
 
-        for i in range(shape1[0]):
+        for i in range(len(arr1)):
             new_row = []
-            for j in range(shape1[1]):
-                new_row.append(arr1[i][j] + arr2[i][j])
-            new_matrix.append(new_row)
+            if len(arr1[i]) == len(arr2[i]):
+                for j in range(len(arr1)):
+                    new_row.append(arr1[i][j] + arr2[i][j])
+                new_matrix.append(new_row)
+            else:
+                return None
         return new_matrix
