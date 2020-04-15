75025980b67353ce7296052eb8727c2525a03f9b
svelezg <svelez.velezgarcia@gmail.com>
2020-04-15 09:31:44 -0500

    Task 5. Across The Planes Missing row length index

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -2,19 +2,16 @@
 """Contain add_matrices2D function"""
 
 
-def add_matrices2D(arr1, arr2):
+def add_matrices2D(mat1, mat2):
     """ adds two matrices element-wise"""
 
-    if len(arr1) != len(arr2):
-        return None
-    else:
+    if len(mat1) == len(mat2):
         new_matrix = []
-
-        for i in range(len(arr1)):
+        for i in range(len(mat1)):
             new_row = []
-            if len(arr1[i]) == len(arr2[i]):
-                for j in range(len(arr1)):
-                    new_row.append(arr1[i][j] + arr2[i][j])
+            if len(mat1[i]) == len(mat2[i]):
+                for j in range(len(mat1[i])):
+                    new_row.append(mat1[i][j] + mat2[i][j])
                 new_matrix.append(new_row)
             else:
                 return None
