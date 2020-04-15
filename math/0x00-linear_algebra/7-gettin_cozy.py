88ef4304e81561ecb8d8fdcd295089602ec81c72
svelezg <svelez.velezgarcia@gmail.com>
2020-04-15 09:37:33 -0500

    Task 7. Gettin’ Cozy Dimension check

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,6 +6,12 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two matrices along a specific axis:"""
     matrix = []
 
+    if axis == 0 and len(mat1[0]) != len(mat2[0]):
+        return None
+
+    if axis == 1 and len(mat1) != len(mat2):
+        return None
+
     for i in range(len(mat1)):
         row = []
         for j in range(len(mat1[0])):
