3992245d35f171e7cba977008748f5248093f10c
svelezg <svelez.velezgarcia@gmail.com>
2020-04-15 09:15:26 -0500

    Task 4. Line Up

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -2,42 +2,17 @@
 """Contain add_arrays function"""
 
 
-def matrix_shape(matrix):
-    """calculates the shape of a matrix"""
-    if matrix:
-        size = [len(matrix)]
-        return matrix_shape_recursion(matrix, size)
-    else:
-        return 0
-
-
-def matrix_shape_recursion(matrix, size):
-    """recursion to calculates the shape of a matrix"""
-    if type(matrix) == int:
-        return size
-    else:
-        for element in matrix:
-            new = element
-            """print("current matrix {}".format(element))"""
-            if type(element) != int:
-                current_len = len(element)
-                size.append(current_len)
-            break
-        return (matrix_shape_recursion(new, size))
-
-
 def add_arrays(arr1, arr2):
     """adds two arrays element-wise"""
 
-    shape1 = matrix_shape(arr1)
-    shape2 = matrix_shape(arr2)
+    shape1 = len(arr1)
+    shape2 = len(arr2)
 
-
-    if shape1[0] != shape2[0]:
+    if shape1 != shape2:
         return None
     else:
         new_array = []
 
-        for i in range(shape1[0]):
+        for i in range(shape1):
             new_array.append(arr1[i] + arr2[i])
         return new_array
