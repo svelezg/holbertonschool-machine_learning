58c41dffb637fdba6b2c9055b9ba75eced55c3e3
svelezg <svelez.velezgarcia@gmail.com>
2020-04-24 09:33:19 -0500

    Task 17 Comment steps Added trimming loop for zero coef at the end

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,27 +4,35 @@
 
 def poly_integral(poly, C=0):
     """calculates the integral of a polynomial"""
-    if len(poly) == 0:
+    "0. C checks"
+    if not isinstance(C, (int, float)):
         return None
 
+    "1. poly checks"
     if type(poly) != list:
         return None
-
+    if len(poly) == 0:
+        return None
+    if poly == [0]:
+        return [C]
     for element in poly:
         if not isinstance(element, (int, float)):
             return None
 
-    if not isinstance(C, (int, float)):
-        return None
-
+    "2. iteration"
     deg = len(poly)
-
     integral = [C]
-
-    for i in range(0, deg):
+    for i in range(deg):
         coef_integral = poly[i] / (i + 1)
         if coef_integral % 1 == 0:
             coef_integral = int(coef_integral)
         integral.append(coef_integral)
+    index = len(integral) - 1
 
+    "3. Trim zeros at the end"
+    while integral[index] == 0:
+        integral.pop(index)
+        i -= 1
+
+    "4. Return Trimmed list"
     return integral
