117f5eee9e68eeb5a3896eb10ddd81448b2a4c4f
svelezg <svelez.velezgarcia@gmail.com>
2020-04-27 08:05:43 -0500

    First Commit README.md

--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -2,13 +2,18 @@
 
 poly_integral = __import__('17-integrate').poly_integral
 
+poly = [0]
+print(poly_integral(poly))
+
+print('*********')
+
+
 poly = [5, 3, 0, 1]
 print(poly_integral(poly))
 
 print('*********')
 
-poly = []
-print(poly_integral(poly, 3))
+
 
 print('*********')
 
