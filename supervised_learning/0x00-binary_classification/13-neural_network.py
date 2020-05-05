60882bbfd5058d152c961ed1eaaa719d8553fb97
svelezg <svelez.velezgarcia@gmail.com>
2020-05-05 14:56:54 -0500

    13-neural_network.py dW1 matmul order

--- supervised_learning/0x00-binary_classification/13-neural_network.py
+++ supervised_learning/0x00-binary_classification/13-neural_network.py
@@ -138,13 +138,13 @@ class NeuralNetwork:
         """
         m = Y.shape[1]
         dZ2 = A2 - Y
-        dW2 = (np.matmul(A1, dZ2.T)) / m
+        dW2 = np.matmul(A1, dZ2.T) / m
         db2 = np.sum(dZ2, axis=1, keepdims=True) / m
         self.__b2 = self.__b2 - alpha * db2
         self.__W2 = self.__W2 - (alpha * dW2).T
 
         dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
-        dW1 = (np.matmul(X, dZ1.T)) / m
+        dW1 = np.matmul(dZ1, X.T) / m
         db1 = np.sum(dZ1, axis=1, keepdims=True) / m
         self.__b1 = self.__b1 - alpha * db1
         self.__W1 = self.__W1 - (alpha * dW1).T
