7ebcd916bbca51c836315a5ac1dae34dd5119573
svelezg <svelez.velezgarcia@gmail.com>
2020-05-05 15:07:01 -0500

    13-neural_network.py fix self.__W1

--- supervised_learning/0x00-binary_classification/13-neural_network.py
+++ supervised_learning/0x00-binary_classification/13-neural_network.py
@@ -140,11 +140,15 @@ class NeuralNetwork:
         dZ2 = A2 - Y
         dW2 = np.matmul(A1, dZ2.T) / m
         db2 = np.sum(dZ2, axis=1, keepdims=True) / m
-        self.__b2 = self.__b2 - alpha * db2
-        self.__W2 = self.__W2 - (alpha * dW2).T
 
-        dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dZ1a = np.matmul(self.__W2.T, dZ2)
+        dZ1b = A1 * (1 - A1)
+        dZ1 = dZ1a * dZ1b
         dW1 = np.matmul(dZ1, X.T) / m
         db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+
+        self.__W2 = self.__W2 - (alpha * dW2).T
+        self.__b2 = self.__b2 - alpha * db2
+
         self.__b1 = self.__b1 - alpha * db1
-        self.__W1 = self.__W1 - (alpha * dW1).T
+        self.__W1 = self.__W1 - (alpha * dW1)
