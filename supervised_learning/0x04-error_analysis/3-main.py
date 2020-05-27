b7362bba2f32df4e93989d6f9f452efb65c4753d
svelezg <svelez.velezgarcia@gmail.com>
2020-05-27 15:40:12 -0500

    Alternative 3-especificity.py

--- supervised_learning/0x04-error_analysis/3-main.py
+++ supervised_learning/0x04-error_analysis/3-main.py
@@ -3,9 +3,10 @@
 import numpy as np
 
 specificity = __import__('3-specificity').specificity
-
+specificity1 = __import__('3b-specificity').specificity
 if __name__ == '__main__':
     confusion = np.load('confusion.npz')['confusion']
 
     np.set_printoptions(suppress=True)
     print(specificity(confusion))
+    print(specificity1(confusion))
---
+++ supervised_learning/0x04-error_analysis/3b-specificity.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Contains the specificity function"""
+
+import numpy as np
+
+
+def specificity(confusion):
+    """
+    calculates the specificity for each class in a confusion matrix:
+    :param confusion: is a confusion numpy.ndarray of shape (classes, classes)
+    where row indices represent the correct
+    labels and column indices represent the predicted labels
+    :return: numpy.ndarray of shape (classes,)
+    containing the specificity of each class
+    """
+
+    total = np.sum(confusion)
+    true_positive = np.diagonal(confusion)
+
+    actual = np.sum(confusion, axis=1)
+    predicted = np.sum(confusion, axis=0)
+
+    false_positive = predicted - true_positive
+    false_negative = actual - true_positive
+    true_negative = total - (false_positive + false_negative + true_positive)
+
+    spec = true_negative / (true_negative + false_positive)
+
+    return spec
