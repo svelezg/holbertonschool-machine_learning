0697ac374335a93b49c7603731ef684ccbbcf533
svelezg <svelez.velezgarcia@gmail.com>
2020-06-02 07:22:27 -0500

    Task 1 Import tensorflow.keras Refactor

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Contains the build_model function"""
 
-import tensorflow as tf
+import tensorflow.keras as keras
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
@@ -16,25 +16,27 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     :param keep_prob: probability that a node will be kept for dropout
     :return: keras model
     """
-    # Returns an input placeholder
-    inputs = tf.keras.Input(shape=(nx,))
+    # input placeholder
+    inputs = keras.Input(shape=(nx,))
 
-    reg = tf.keras.regularizers.L1L2(l2=lambtha)
+    # regularization scheme
+    reg = keras.regularizers.L1L2(l2=lambtha)
 
-    # A layer instance is callable on a tensor, and returns a tensor.
-    # Adds first densely-connected layer to the model:
-    my_layer = tf.keras.layers.Dense(units=layers[0],
-                                     activation=activations[0],
-                                     kernel_regularizer=reg,
-                                     input_shape=(nx,))(inputs)
+    # a layer instance is callable on a tensor, and returns a tensor.
+    # first densely-connected layer
+    my_layer = keras.layers.Dense(units=layers[0],
+                                  activation=activations[0],
+                                  kernel_regularizer=reg,
+                                  input_shape=(nx,))(inputs)
 
-    # Add subsequent densely-connected layers:
+    # subsequent densely-connected layers:
     for i in range(1, len(layers)):
-        my_layer = tf.keras.layers.Dropout(1 - keep_prob)(my_layer)
-        my_layer = tf.keras.layers.Dense(units=layers[i],
-                                         activation=activations[i],
-                                         kernel_regularizer=reg,
-                                         )(my_layer)
+        my_layer = keras.layers.Dropout(1 - keep_prob)(my_layer)
+        my_layer = keras.layers.Dense(units=layers[i],
+                                      activation=activations[i],
+                                      kernel_regularizer=reg,
+                                      )(my_layer)
 
-    model = tf.keras.Model(inputs=inputs, outputs=my_layer)
-    return model
+    network = keras.Model(inputs=inputs, outputs=my_layer)
+
+    return network
