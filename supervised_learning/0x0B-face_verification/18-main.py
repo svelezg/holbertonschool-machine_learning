0ac0dd3233508cd1780d05984b0d442ddd944ab4
svelezg <svelez.velezgarcia@gmail.com>
2020-07-09 10:29:49 -0500

    Verify test

--- supervised_learning/0x0B-face_verification/18-main.py
+++ supervised_learning/0x0B-face_verification/18-main.py
@@ -26,7 +26,7 @@ database = np.array(embedded)
 fv = FaceVerification('models/trained_fv.h5', database, identities)
 
 # image selection
-my_image = random.choice(images)
+my_image = load_images('TESTaligned', as_array=True)[0]
 
 identity, distance = fv.verify(my_image, 0.06090909090909092)
 print(identity, distance)
