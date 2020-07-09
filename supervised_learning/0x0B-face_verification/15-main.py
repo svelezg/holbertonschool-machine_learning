0d89882a2eb029cd4eb9cb08edb74b4b6cb61d22
svelezg <svelez.velezgarcia@gmail.com>
2020-07-09 09:34:44 -0500

    15-main.py

--- supervised_learning/0x0B-face_verification/15-main.py
+++ supervised_learning/0x0B-face_verification/15-main.py
@@ -10,7 +10,7 @@ images, filenames = load_images('HBTNaligned', as_array=True)
 identities = [re.sub('[0-9]', '', f[:-4]) for f in filenames]
 print(set(identities))
 thresholds = np.linspace(0.01, 0.1, 100)
-tm = TrainModel('models/face_verification.h5', 0.2)
+tm = TrainModel('models/trained_fv.h5', 0.2)
 tau, f1, acc = tm.best_tau(images, identities, thresholds)
 print(tau)
 print(f1)
