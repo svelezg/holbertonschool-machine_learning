01c72e28ea43e5fc039f97b3ccbe1bac6f4bc696
svelezg <svelez.velezgarcia@gmail.com>
2020-07-15 10:18:19 -0500

    task 1 intermediate variable before return

--- supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
@@ -10,7 +10,8 @@ class NST:
     NST class performs tasks for neural style transfer
     """
     style_layers = ['block1_conv1', 'block2_conv1',
-                    'block3_conv1', 'block4_conv1', 'block5_conv1']
+                    'block3_conv1', 'block4_conv1',
+                    'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
@@ -114,4 +115,6 @@ class NST:
         model_outputs = style_outputs + [content_outputs]
 
         # Build model
-        return tf.keras.models.Model(vgg.input, model_outputs)
+        model = tf.keras.models.Model(vgg.input, model_outputs)
+
+        return model
