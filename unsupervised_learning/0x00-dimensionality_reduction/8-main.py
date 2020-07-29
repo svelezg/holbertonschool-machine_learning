96635865223dc090da2f33c4f755f176a281554b
svelezg <svelez.velezgarcia@gmail.com>
2020-07-29 14:37:12 -0500

    Task 8 correction change sign before learning rate normalization of Y in each iteration

--- unsupervised_learning/0x00-dimensionality_reduction/8-main.py
+++ unsupervised_learning/0x00-dimensionality_reduction/8-main.py
@@ -7,7 +7,7 @@ tsne = __import__('8-tsne').tsne
 np.random.seed(0)
 X = np.loadtxt("mnist2500_X.txt")
 labels = np.loadtxt("mnist2500_labels.txt")
-Y = tsne(X, perplexity=50.0, iterations=3000, lr=750)
+Y = tsne(X, perplexity=50.0, iterations=1000, lr=750)
 plt.scatter(Y[:, 0], Y[:, 1], 20, labels)
 plt.colorbar()
 plt.title('t-SNE')
--- unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
+++ unsupervised_learning/0x00-dimensionality_reduction/8-tsne.py
@@ -25,26 +25,29 @@ def tsne(X, ndims=2, idims=50, perplexity=30.0, iterations=1000, lr=500):
     """
     X = pca(X, idims)
 
-    momentum_i = 0.5
-    momentum_f = 0.8
     n, d = X.shape
 
-    P = 4 * P_affinities(X)
+    # early exaggeration with an exaggeration of 4
+    P = P_affinities(X, perplexity=perplexity) * 4
 
     Y = np.random.randn(n, ndims)
     Yprev = Y
 
     for i in range(1, iterations + 1):
         dY, Q = grads(Y, P)
+
+        # momentum
         if i <= 20:
-            momentum = momentum_i
+            momentum = 0.5
         else:
-            momentum = momentum_f
+            momentum = 0.8
 
         # update
         tmp = Y
-        Y = Y + lr * dY + momentum * (Y - Yprev)
+
+        Y = Y - lr * dY + momentum * (Y - Yprev)
         Yprev = tmp
+        Y = Y - np.mean(Y, axis=0)
 
         if i != 0 and i % 100 == 0:
             C = cost(P, Q)
