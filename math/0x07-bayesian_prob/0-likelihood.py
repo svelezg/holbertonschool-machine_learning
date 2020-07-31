6cd9bad4ea6a14448d6f8b540e4865d15e1890c4
svelezg <svelez.velezgarcia@gmail.com>
2020-07-31 11:44:36 -0500

    Task 0 to 4

---
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""contains the likelihood function"""
+
+import numpy as np
+
+
+def likelihood(x, n, P):
+    """
+    calculates the likelihood of obtaining this data
+    given various hypothetical probabilities of
+    developing severe side effects
+    :param x: number of patients that develop severe side effects
+    :param n: total number of patients observed
+    :param P: 1D numpy.ndarray containing the various hypothetical
+        probabilities of developing severe side effects
+    :return: 1D numpy.ndarray containing the likelihood of obtaining the data,
+        x and n, for each probability in P
+    """
+    if x > n:
+        err = 'x cannot be greater than n'
+        raise ValueError(err)
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        err = 'P must be a 1D numpy.ndarray'
+        raise TypeError(err)
+
+    if np.any(P < 0) or np.any(P > 1):
+        err = 'All values in P must be in the range [0, 1]'
+        raise ValueError(err)
+
+    coef = np.math.factorial(n) / \
+        (np.math.factorial(x) * np.math.factorial(n - x))
+
+    result = coef * (P ** x) * (1 - P) ** (n - x)
+
+    return result
---
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""contains the intersection function"""
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """
+    calculates the intersection of obtaining this data
+    with the various hypothetical probabilities
+    :param x: number of patients that develop severe side effects
+    :param n: total number of patients observed
+    :param P: 1D numpy.ndarray containing the various hypothetical
+        probabilities of developing severe side effects
+    :param Pr: 1D numpy.ndarray containing the prior beliefs of P
+    :return: D numpy.ndarray containing the intersection of obtaining x
+        and n with each probability in P
+    """
+    if not isinstance(n, int) or n <= 0:
+        err = 'n must be a positive integer'
+        raise ValueError(err)
+
+    if not isinstance(x, int) or n < 0:
+        err = 'x must be an integer that is greater than or equal to 0'
+        raise ValueError(err)
+
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        err = 'P must be a 1D numpy.ndarray'
+        raise TypeError(err)
+
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        err = 'Pr must be a numpy.ndarray with the same shape as P'
+        raise TypeError(err)
+
+    if np.any(P < 0) or np.any(P > 1) or np.any(Pr < 0) or np.any(P > 1):
+        err = 'All values in {P} must be in the range [0, 1] ' \
+              'where {P} is the incorrect variable'
+        raise ValueError(err)
+
+    test = np.isclose([np.sum(Pr)], [1.])
+
+    if not test[0]:
+        err = 'Pr must sum to 1'
+        raise ValueError(err)
+
+    coef = np.math.factorial(n) /\
+        (np.math.factorial(x) * np.math.factorial(n - x))
+
+    likelihood = coef * (P ** x) * (1 - P) ** (n - x)
+
+    result = likelihood * Pr
+
+    return result
