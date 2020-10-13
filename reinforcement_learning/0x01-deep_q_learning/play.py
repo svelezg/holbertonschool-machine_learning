00cdc1ed24473b1d37926f1da922e8e3ee783b19
svelezg <svelez.velezgarcia@gmail.com>
2020-10-13 12:57:51 -0500

    deep q learning

---
+++ reinforcement_learning/0x01-deep_q_learning/play.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+display a game played by the agent trained by train.py
+"""
+
+import gym
+from rl.agents.dqn import DQNAgent
+from rl.memory import SequentialMemory
+import keras as K
+
+create_q_model = __import__('train').create_q_model
+AtariProcessor = __import__('train').AtariProcessor
+
+
+if __name__ == '__main__':
+    env = gym.make("Breakout-v0")
+    env.reset()
+    num_actions = env.action_space.n
+    window = 4  # number of screenshots
+
+    # CREATE MODEL
+    # deep convolutional neural network model
+    model = create_q_model(num_actions, window)
+    memory = SequentialMemory(limit=1000000, window_length=window)
+    processor = AtariProcessor()
+
+    dqn = DQNAgent(model=model,
+                   nb_actions=num_actions,
+                   processor=processor,
+                   memory=memory)
+
+    dqn.compile(K.optimizers.Adam(lr=.00025), metrics=['mae'])
+
+    # load weights.
+    dqn.load_weights('policy.h5')
+
+    # evaluate algorithm for 10 episodes.
+    dqn.test(env, nb_episodes=10, visualize=True)
