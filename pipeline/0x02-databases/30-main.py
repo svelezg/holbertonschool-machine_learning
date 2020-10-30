5c340d9d2b88b2e59113034319be240a90e4c541
svelezg <svelez.velezgarcia@gmail.com>
2020-10-30 17:59:13 -0500

    Task 30 to 34

--- pipeline/0x02-databases/30-main.py
+++ pipeline/0x02-databases/30-main.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 list_all = __import__('30-all').list_all
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient('mongodb://127.0.0.1:27017/')
     school_collection = client.my_db.school
     schools = list_all(school_collection)
     for school in schools:
