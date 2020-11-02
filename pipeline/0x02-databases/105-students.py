003b360e7bcc8f7a897c4ca7ffa958a5a5577d10
svelezg <svelez.velezgarcia@gmail.com>
2020-11-02 15:11:52 -0500

    Task 104 and 105

---
+++ pipeline/0x02-databases/105-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" contains the top_students function"""
+
+
+def top_students(mongo_collection):
+    """
+    students sorted by average score
+    :param mongo_collection:
+    :return:
+    """
+    students = mongo_collection.find()
+    students_list = []
+    for student in students:
+        topics = student["topics"]
+
+        score = 0
+        for topic in topics:
+            score += topic["score"]
+        score /= len(topics)
+        student["averageScore"] = score
+        students_list.append(student)
+
+    return sorted(students_list, key=lambda i: i["averageScore"], reverse=True)
