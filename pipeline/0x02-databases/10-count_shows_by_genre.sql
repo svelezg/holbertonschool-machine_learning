9493c033ce63d92848cb8d3177e5445ef7f8d09d
svelezg <svelez.velezgarcia@gmail.com>
2020-11-02 14:45:47 -0500

    Task 10

--- pipeline/0x02-databases/10-count_shows_by_genre.sql
+++ pipeline/0x02-databases/10-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
--- lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- lists all genres from hbtn_0d_tvshows and
+-- displays the number of shows linked to each
 SELECT tv_genres.name as genre,
 COUNT(tv_show_genres.show_id) as number_of_shows
 FROM tv_genres
